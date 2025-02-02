f77635722622a263b6a8a9e427cd900c7a745226
Bishes7 <specialbishes9@gmail.com>
2025-02-02 21:07:32 +1100

    practise project started for api

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`http://localhost:${PORT}`);
+});
